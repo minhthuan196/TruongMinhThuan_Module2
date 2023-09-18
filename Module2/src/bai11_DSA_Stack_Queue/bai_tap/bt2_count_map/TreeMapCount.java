9bb6b251604a7d9bd341af29586d53524cfc664a
minhthuan196 <minhthuandtu@gmail.com>
2023-09-18 20:39:52 +0700

    update

--- Module2/src/bai11_DSA_Stack_Queue/bai_tap/bt2_count_map/TreeMapCount.java
+++ Module2/src/bai11_DSA_Stack_Queue/bai_tap/bt2_count_map/TreeMapCount.java
@@ -4,11 +4,20 @@ import java.util.TreeMap;
 
 public class TreeMapCount {
     public static void main(String[] args) {
-        TreeMap<String, Integer> treeMap = new TreeMap<String, Integer>();
-        String paragraph = "Truong Minh Thuan Truong Minh Thuan";
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+        String paragraph = "Truong Minh Thuan Truong Minh Thuan Truong Minh Thuan";
+        paragraph = paragraph.toLowerCase();
         String[] arrayParagraph = paragraph.split(" ");
-        for (int i = 0; i < arrayParagraph.length; i++) {
+        int key;
+        for (String s : arrayParagraph) {
+            if (treeMap.containsKey(s)) {
+                key = treeMap.get(s) + 1;
+                treeMap.put(s, key);
+            } else {
+                treeMap.put(s, 1);
+            }
         }
+        System.out.println(treeMap);
     }
 
 
